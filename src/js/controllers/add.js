d853738b104147c767e0f1b329e44c35f4170e2f
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-23 19:27:35 -0500

    update/add page styling and nav styling

--- src/js/controllers/add.js
+++ src/js/controllers/add.js
@@ -2,11 +2,56 @@ const SERVER = 'https://hackathon-backend.herokuapp.com/posts/'
 
 
 function addController ($scope, $http, $state) {
+
+	$scope.valid ={};
+	$scope.class={};
+	$scope.class.submit = "";
+
+
   $scope.addImage = (image) => {
-    $http.post(SERVER, image).then((resp) => {
-      $state.go('home');
-    });
+  	if ($scope.valid.submit){
+	    $http.post(SERVER, image).then((resp) => {
+	      $state.go('home');
+	    });
+	}
   };
+
+  $scope.validName = (string) => {
+  	if (string){
+  		$scope.valid.name = true;
+  	} else {
+  		$scope.valid.name = false;
+  	}
+  	$scope.validAll();
+  }
+
+  $scope.validUrl = (string) => {
+  	if (string){
+  		$scope.valid.destination_url = true;
+  	} else {
+  		$scope.valid.destination_url = false;
+  	}
+  	$scope.validAll();
+  }
+
+  $scope.validDescription = (string) => {
+  	if (string){
+  		$scope.valid.description = true;
+  	} else {
+  		$scope.valid.description = false;
+  	}
+   	$scope.validAll();
+  }
+
+  $scope.validAll = function() {
+  		if ($scope.valid.name && $scope.valid.destination_url && $scope.valid.description){
+  			$scope.valid.submit = true;
+  			$scope.class.submit = "big-eye"
+  		} else {
+  			$scope.valid.submit = false;
+  			$scope.class.submit = "";
+  		}
+  }
 
 };
 
--- src/js/controllers/update.js
+++ src/js/controllers/update.js
@@ -8,6 +8,14 @@ function updateController ($scope, $http, $stateParams, $state, $location) {
   		$scope.update.title = resp.data.title;
   		$scope.update.destination_url = resp.data.destination_url;
   		$scope.update.description = resp.data.description;
+
+      $scope.valid ={};
+      $scope.valid.name = true;
+      $scope.valid.destination_url = true;
+      $scope.valid.description= true;
+      $scope.valid.submit = true;
+      $scope.class={};
+      $scope.class.submit = "big-eye";
     });
 
   }
@@ -15,10 +23,51 @@ function updateController ($scope, $http, $stateParams, $state, $location) {
 
 
   $scope.updateImage = (image) => {
-    $http.patch(SERVER + $stateParams.id, image).then((resp) => {
-      $location.path('details/' + $stateParams.id)
-    });
+
+    if ($scope.valid.submit){
+      $http.patch(SERVER + $stateParams.id, image).then((resp) => {
+        $location.path('details/' + $stateParams.id)
+      });
+    }
   };
+
+    $scope.validName = (string) => {
+    if (string){
+      $scope.valid.name = true;
+    } else {
+      $scope.valid.name = false;
+    }
+    $scope.validAll();
+  }
+
+  $scope.validUrl = (string) => {
+    if (string){
+      $scope.valid.destination_url = true;
+    } else {
+      $scope.valid.destination_url = false;
+    }
+    $scope.validAll();
+  }
+
+  $scope.validDescription = (string) => {
+    if (string){
+      $scope.valid.description = true;
+    } else {
+      $scope.valid.description = false;
+    }
+    $scope.validAll();
+  }
+
+  $scope.validAll = function() {
+      if ($scope.valid.name && $scope.valid.destination_url && $scope.valid.description){
+        $scope.valid.submit = true;
+        $scope.class.submit = "big-eye";
+      } else {
+        $scope.valid.submit = false;
+        $scope.class.submit = "";
+      }
+      console.log($scope.valid.submit, $scope.class.submit)
+  }
 
 };
 
